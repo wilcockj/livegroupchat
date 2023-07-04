0954704fb70bc48856e820e5975fe5750c0e1807
James Wilcock <jameswilcock8@gmail.com>
2023-07-03 20:18:46 -0700

    format server js

--- server.js
+++ server.js
@@ -1,50 +1,54 @@
-const WebSocket = require('ws');
-const https = require('https');
-const http = require('http');
-const fs = require('fs');
-const express = require('express');
+const WebSocket = require("ws");
+const https = require("https");
+const http = require("http");
+const fs = require("fs");
+const express = require("express");
 const app = express();
 // Variable to keep track of active connections
 let activeConnections = 0;
-const PORT = process.env.NODE_ENV === 'production' ? 443 : 8089;
+const PORT = process.env.NODE_ENV === "production" ? 443 : 8089;
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   console.log("starting dev server");
-  app.use(express.static('public'));
+  app.use(express.static("public"));
   var server = http.createServer(app);
 }
 
-if (process.env.NODE_ENV === 'production'){
-    console.log("starting production server");
-    app.use(express.static('public'));
-    const serverOptions = {
-      cert : fs.readFileSync('/etc/letsencrypt/live/swiftnotes.net/fullchain.pem'),
-      key : fs.readFileSync('/etc/letsencrypt/live/swiftnotes.net/privkey.pem'),
-    };
-    var server = https.createServer(serverOptions, app);
+if (process.env.NODE_ENV === "production") {
+  console.log("starting production server");
+  app.use(express.static("public"));
+  const serverOptions = {
+    cert: fs.readFileSync("/etc/letsencrypt/live/swiftnotes.net/fullchain.pem"),
+    key: fs.readFileSync("/etc/letsencrypt/live/swiftnotes.net/privkey.pem"),
+  };
+  var server = https.createServer(serverOptions, app);
 }
 
+app.get("/", (req, res) => res.sendFile(`/index.html`));
 
-app.get("/", (req, res) => res.sendFile(`/index.html`))
+const wss = new WebSocket.WebSocketServer({ server });
 
-
-const wss = new WebSocket.WebSocketServer({server});
-
-wss.on('connection', (ws,req) => {
+wss.on("connection", (ws, req) => {
   activeConnections++;
-  const memused = process.memoryUsage().rss / 1024 / 1024
-  console.log('WebSocket connection established. Now have', activeConnections, 'connections, using' , memused, 'MB');
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const memused = process.memoryUsage().rss / 1024 / 1024;
+  console.log(
+    "WebSocket connection established. Now have",
+    activeConnections,
+    "connections, using",
+    memused,
+    "MB"
+  );
+  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
   console.log("Came from", ip);
-  ws.on('message', (message) => {
+  ws.on("message", (message) => {
     var strmessage = String.fromCodePoint(...message);
-    console.log('Message in string form:', strmessage);
+    console.log("Message in string form:", strmessage);
     // Process the received message here or broadcast it to other connected
     // clients
-    if (strmessage == '__ping__'){
-        console.log("sending pong to ", ip);
-        ws.send('__pong__');
-        return;
+    if (strmessage == "__ping__") {
+      console.log("sending pong to ", ip);
+      ws.send("__pong__");
+      return;
     }
     console.log("Sending message to", wss.clients.size, "clients");
     wss.clients.forEach((client) => {
@@ -54,17 +58,18 @@ wss.on('connection', (ws,req) => {
     });
   });
 
-  ws.on('close', () => { 
-      activeConnections--;
-      console.log('WebSocket connection closed.');
-    });
+  ws.on("close", () => {
+    activeConnections--;
+    console.log("WebSocket connection closed.");
+  });
 });
 
 server.listen(PORT, console.log(`server running on port ${PORT}`));
-http.createServer(function(req, res) {
-      res.writeHead(301,
-                    {"Location" : "https://" + req.headers['host'] + req.url});
-      res.end();
-    })
-    .listen(80);
-
+http
+  .createServer(function (req, res) {
+    res.writeHead(301, {
+      Location: "https://" + req.headers["host"] + req.url,
+    });
+    res.end();
+  })
+  .listen(80);
